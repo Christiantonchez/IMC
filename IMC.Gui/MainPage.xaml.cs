48384a7eee4d18d7570cfb65f228b876a2c2e77e
christian tonchez <christian16alexander@gmail.com>
2025-10-07 04:39:18 -0600

    Calculador de IMC.NET MAUI

---
+++ IMC.Gui/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿namespace IMC.Gui
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            LimpiarInformacion();
+        }
+
+        private void LimpiarInformacion()
+        {
+            PesoEntry.Text = string.Empty;
+            EstaturaEntry.Text = string.Empty;
+            ImcLabel.Text = string.Empty;
+            SituacionNutricionalLabel.Text = string.Empty;
+        }
+
+        private void Calcular_Button_Clicked(object sender, EventArgs e)
+        {
+            decimal peso = Convert.ToDecimal(PesoEntry.Text);
+            decimal estatura = Convert.ToDecimal(EstaturaEntry.Text);
+            decimal imc = IndiceDeMasaCorporal(peso, estatura);
+            ImcLabel.Text = imc.ToString("G6");
+
+            string estadoNutricional = string.Empty;
+
+            if (imc < 18.5M)
+            {
+                estadoNutricional = "peso bajo";
+            }
+            if (imc < 25.0M)
+            {
+                estadoNutricional = "peso normal";
+
+            }
+
+            if (imc < 30.0M)
+            {
+                estadoNutricional = "sobre peso";
+            }
+
+            if (imc < 40.0M)
+            {
+                estadoNutricional = "obesidad";
+            }
+            else
+            {
+                estadoNutricional = "Obesidad extrema";
+            }
+
+            SituacionNutricionalLabel.Text = SituacionNutricional(imc);
+        }
+
+        private static decimal IndiceDeMasaCorporal(decimal peso, decimal estatura)
+        {
+            return peso / (estatura * estatura);
+        }
+
+        private void Limpiar_Button_Clicked(object sender, EventArgs e)
+        {
+            LimpiarInformacion();
+        }
+
+       private static string SituacionNutricional(decimal imc)
+        {
+            string estadoNutricional;
+            if (imc < 18.5M)
+            {
+                 estadoNutricional = "peso bajo";
+            }
+            if (imc < 25.0M)
+            {
+                estadoNutricional = "peso normal";
+
+            }
+
+            if (imc < 30.0M)
+            {
+                estadoNutricional = "sobre peso";
+            }
+
+            if (imc < 40.0M)
+            {
+                estadoNutricional = "obesidad";
+            }
+            else
+            {
+                estadoNutricional = "Obesidad extrema";
+            }
+
+            return estadoNutricional;
+
+        }
+    }
+}
